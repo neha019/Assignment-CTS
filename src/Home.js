daf0ce816ac526a48d05b0cc94040cde246ec891
Neha Verma <nehav.verma019@gmail.com>
2021-07-29 09:54:30 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-import { Input, Button } from "reactstrap";
+import { Button } from "reactstrap";
 import './Home.css';
+
+var DropDown = [];
+var RadioButton = [];
+var obj;
+var id = [];
+var fieldType = [];
+var label = [];
 
 class Home extends Component {
     constructor() {
@@ -11,38 +18,11 @@ class Home extends Component {
             buttonClick: false,
             name: '',
             email: '',
+            phoneNumber: '',
             select: 'Select',
-            option: [
-                {
-                    label: "Bangalore",
-                    value: "Bangalore",
-                },
-                {
-                    label: "Gurgaon",
-                    value: "Gurgaon",
-                },
-                {
-                    label: "Mysore",
-                    value: "Mysore",
-                },
-                {
-                    label: "Kolkata",
-                    value: "Kolkata",
-                },
-                {
-                    label: "Delhi",
-                    value: "Delhi",
-                },
-            ],
-            radioSource: [
-                {
-                    value: 'Metro'
-                },
-                {
-                    value: 'Non-Metro'
-                }
-            ],
-            phoneNumber: ''
+
+
+
         }
     }
 
@@ -62,32 +42,50 @@ class Home extends Component {
         })
     }
 
+    radioButton = () => {
+        RadioButton.map((radio) => (
+            { radio }
+        ))
+    }
+
+    dropdownSelection = () => {
+        DropDown.map((option) => (
+            <option value={option} key={option}>{option}</option>
+
+        ))
+    }
+
     getEvaluation = () => {
         // console.log(this.state.textAreaValue);
-        var arr = [];
-        var phone = document.getElementById('phoneNumber');
-        console.log(phone)
-        var parsedJSON = JSON.parse(JSON.stringify(this.state.textAreaValue));
-        console.log(parsedJSON)
-        arr.push(parsedJSON)
-        console.log(arr)
+        // DropDown = [];
+        // RadioButton = [];
+        obj = '';
+        console.log(this.state.textAreaValue)
+        this.setState({
+            textAreaValue: ''
+        })
+        obj = Function('return (' + this.state.textAreaValue + ')')();
+        console.log(obj)
+        // var str = this.state.textAreaValue.replace('//g', '').split('\n').map((ele, ind) => console.log(ele[ind]))
+        console.log(obj.forEach(ele => console.log(ele.fieldType)))
         this.setState({ buttonClick: true })
-        // console.log(this.state.textAreaValue)
-        if (this.state.phoneNumber !== '') {
-            console.log(this.state.name, this.state.email)
+        if (obj) {
+            obj.forEach(ele => {
+                console.log(ele.DataSource, ele.name, ele.label, ele.fieldType);
 
-            if (phone) {
+                id.push(ele.name);
+                label.push(ele.label);
+                fieldType.push(ele.fieldType);
 
-                arr.push({
-                    'name': phone.getAttribute('name'),
-                    'label': document.getElementById('phone').textContent,
+                if (ele.DataSource !== undefined && ele.fieldType === 'DropDown') {
+                    ele.DataSource.forEach(source => DropDown.push(source))
+                }
+                if (ele.DataSource !== undefined && ele.fieldType === 'RadioButton') {
+                    ele.DataSource.forEach(source => RadioButton.push(source))
+                }
 
-                })
-                this.setState({ textAreaValue: JSON.stringify(arr).replace('/\n\t/g', '\n').
-                replace('/\n/g', '').replace('/\/g', '\n').replace('/\t/g', '\n').split('\n')})
-                console.log(JSON.parse(JSON.stringify(arr[0] + arr[1])))
-            }
-
+            });
+            this.setState({ textAreaValue: this.state.textAreaValue })
         }
 
     }
@@ -99,44 +97,56 @@ class Home extends Component {
     }
 
     render() {
+        var count = 0;
         return (
+
             <div className="divBlock">
                 <label>JSON : </label>
                 {/* <textarea cols="20" rows="6" value={this.state.textAreaValue} onChange={this.handleChange}></textarea> */}
-                <Input id="textArea" name="text" type="textarea" value={this.state.textAreaValue} onChange={this.handleChange}></Input>
+                <textarea id="textArea" name="text" rows={50} cols={20} value={this.state.textAreaValue} onChange={this.handleChange}></textarea>
                 {' '}
-                <Button variant="primary" onClick={this.getEvaluation}>
+                <Button key="unique" variant="primary" onClick={this.getEvaluation}>
                     {'Evaluate'}
                 </Button>
                 {' '}
                 {(this.state.buttonClick === true) && (
                     <div className="userInput">
-                        <label id="username">User Name : </label>
-                        <Input type="text" name="text" value={this.state.name} onChange={(e) => { console.log(e.target.nodeType, e.target.name); this.setState({ name: e.target.value }) }}></Input>
+                        {obj.map(element => (
+                            <>
+                                <label id={element.label}>{element.label + ':'}</label>
+                                <input type={element.fieldType} key={element.name} id={element.name} name={element.name} onChange={this.selectradioOption}/>
+
+
+                                <br />
+                            </>
+                        ))}
+
+
+                        {/* <label id="email">Email : </label>
+                        <input type="email" name="email" value={this.state.email} onChange={(e) => { this.setState({ email: e.target.value }) }}></input>
                         <br />
-                        <label id="email">Email : </label>
-                        <Input type="email" name="email" value={this.state.email} onChange={(e) => { console.log(e.target.name); this.setState({ email: e.target.value }) }}></Input>
-                        <br />
-                        <label id="city">City : </label>
-                        <Input type="select" name="select" value={this.state.select} onChange={this.selectChange}>
+                        <label id="city">City : </label> */}
+                        {/* <input type="select" name="select" onChange={this.selectChange}>
                             {this.state.option.map((option) => (
                                 <option value={option.value} key={option.value}>{option.label}</option>
                             ))}
-                        </Input>
+                        </input>
                         <br />
                         <label id="cityType">City Type : </label>
                         {this.state.radioSource.map((radio) => (
                             <>
-                                <Input id="radioButton" type="radio" name="radio1" value={radio.value} onChange={this.selectradioOption} />
+                                <input id="radioButton" type="radio" name="radio1" value={radio.value} onChange={this.selectradioOption} />
                                 {radio.value}
                             </>
                         ))}
-                        <br />
-                        <label id="phone">Phone Number : </label>
-                        <Input type="text" id="phoneNumber" name="Phone" value={this.state.phoneNumber} onChange={(e) => { console.log(e.target.name); this.setState({ phoneNumber: e.target.value }) }}></Input>
+                        <br /> */}
+                        {/* <label id="phone">Phone Number : </label>
+                        <input type="text" id="phoneNumber" name="Phone" value={this.state.phoneNumber} onChange={(e) => { console.log(e.target.name); this.setState({ phoneNumber: e.target.value }) }}></input> */}
 
                     </div>
                 )}
+
+
             </div>
         )
     }
